1ba4a22627735e5d793067460ff328eafc688c45
Skullking2520 <hyunjunsoo1@gmail.com>
2025-02-24 10:05:31 +0900

    Update vacancy_detail_1.py

--- vacancy_detail_1.py
+++ vacancy_detail_1.py
@@ -254,14 +254,12 @@ def main():
             print(f"Error processing detail: {e}")
             continue
 
-    progress_sheet.update("A3", [[json.dumps({"progress": "setting", "UrlNum": 1})]])
-    progress_sheet.update("A4", [[json.dumps({"progress": "setting", "RowNum": 0})]])
+    progress_sheet.update([[json.dumps({"progress": "setting", "UrlNum": 1})]], "A3")
+    progress_sheet.update([[json.dumps({"progress": "setting", "RowNum": 0})]], "A4")
+    progress_sheet.update([[json.dumps({"progress": "setting", "RowNum": 0})]], "B4")
     va_sheet.update([["Scrapping Finished"]], "Q1")
     driver.quit()
     print("Saved every data into the Google Sheet successfully.")
-
-    if __name__ == "__main__":
-        main()
 
 if __name__ == "__main__":
     main()
