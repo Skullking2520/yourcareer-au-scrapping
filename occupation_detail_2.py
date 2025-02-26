89530c7ce112554d3ccaecf9bb03c13c34283ffb
Skullking2520 <hyunjunsoo1@gmail.com>
2025-02-27 01:13:51 +0900

    Update occupation_detail_2.py

--- occupation_detail_2.py
+++ occupation_detail_2.py
@@ -292,12 +292,10 @@ def main():
                 batch_update_multiple_rows(occ_sheet, pending_updates)
                 pending_updates = []
             progress["progress"] = "finished"
+            ph.save_progress(progress)
         except NoSuchElementException as e:
             print(f"Error processing detail: {e}")
             continue
-
-    progress_sheet.update(values=[[json.dumps({"progress": "setting", "RowNum": 1})]], range_name="B2")
-    occ_sheet.update([["Scrapping Finished"]], "R1")
     driver.quit()
     print("Saved every data into the Google Sheet successfully.")
 
