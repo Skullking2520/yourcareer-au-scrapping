af732fdc5c48a87cf32612201c2bb15172415e28
Skullking2520 <hyunjunsoo1@gmail.com>
2025-03-03 12:11:36 +0900

    Update occ_vac_compile_11.py

--- occ_vac_compile_11.py
+++ occ_vac_compile_11.py
@@ -241,8 +241,8 @@ def main():
                 except Exception as e:
                     print(f"An error occurred while waiting for page load: {e}. Attempt {attempt + 1}")
             else:
-                print(f"Failed to load page {pagenum}, skipping...")
                 progress["RowNum"] += 20
+                print(f"Vacancy elements did not load in time. Skipping row {progress["RowNum"]}")
                 break
 
             vacancy_dict = {str(va[0]): va[1] for va in vac_extracted_list}
